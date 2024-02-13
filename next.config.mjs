2d4e0e9901a8efcf051baf8094fe875cf20a83fe
Your Name <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>
2024-02-13 15:06:22 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+import nextPwaConfig from './next-pwa-config.js';
+
+const nextConfig = {
+  reactStrictMode: true,
+  // Other Next.js configuration options
+};
+
+export default {
+  ...nextPwaConfig,
+  ...nextConfig
+};
